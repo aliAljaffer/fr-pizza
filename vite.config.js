feb101fe3c9e6389a78bd25bda7af4d9e4d889d5
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2024-09-07 18:58:06 +0300

    Build ready

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "./",
+  build: {
+    outDir: "./docs",
+    emptyOutDir: true,
+  },
   plugins: [react(), eslint()],
 });
